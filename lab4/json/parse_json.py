b8ca80701a2e78ca1095ffce87d0809abb00a96e
azamat <az_alipbek@kbtu.kz>
2025-02-20 21:43:08 +0500

    json

---
+++ lab4/json/parse_json.py
@@ -0,0 +1,18 @@
+﻿import json
+def connection():
+    str='''Interface Status
+================================================================================
+DN                                                 Description           Speed    MTU'''
+    print(str)
+    with open(r"C:\Users\Huawei\OneDrive\Рабочий стол\new_folder_2\labs\lab4\json\sample-data.json", "r", encoding="utf-8") as file:
+
+        fromFile=json.load(file)
+        array=fromFile["imdata"]
+        for i in array:
+            tn=i["l1PhysIf"]["attributes"]["dn"]
+            description=i["l1PhysIf"]["attributes"]["descr"]
+            speed=i["l1PhysIf"]["attributes"]["speed"]
+            mtu=i["l1PhysIf"]["attributes"]["mtu"]
+            print(f"{tn}                   {description}            {speed}      {mtu}")
+        file.close()
+connection()
